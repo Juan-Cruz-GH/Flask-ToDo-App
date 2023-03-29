690e844672ac2f224b1a1ba72ac50730cad71cd5
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-03-29 00:43:19 -0300

    Form not used

--- admin/src/web/forms/category.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class CategoryForm(FlaskForm):
-    name = StringField("Name")
-    description = StringField("Description")
-    submit = SubmitField("Submit")
