c5f5670572912bee8b7f985b93381a8823ad6cd1
Juan Cruz Cassera Botta <juancasserab@gmail.com>
2023-03-29 00:43:20 -0300

    Update and create category

--- admin/src/web/controllers/category.py
+++ admin/src/web/controllers/category.py
@@ -1,35 +1,28 @@
 from flask import Blueprint, render_template, request, redirect
 from src.models import category
-from src.web.forms.category import CategoryForm
 
 category_blueprint = Blueprint("categories", __name__, url_prefix="/categories")
 
 
-@category_blueprint.get("/category/<id>")
-def show_category_data(id):
-    kwargs = {"category": category.find_by_id(id)}
-    return render_template("/categories/show_category.html", **kwargs)
-
-
 @category_blueprint.route("/create-category", methods=["GET", "POST"])
 def create_category():
-    form = CategoryForm()
-    if form.validate_on_submit():
-        print("asd")
-        data = {
-            "name": form.name.data,
-            "description": request.form.get("description"),
-            "icon": request.form.get("icon"),
-            "color": request.form.get("color"),
-        }
-        category.create(data)
-        return redirect("/categories/")
-    else:
-        return render_template("/categories/add_category.html", form=form)
+    if request.method == "GET":
+        return render_template("/categories/add_category.html")
+    data = {
+        "name": request.form.get("name"),
+        "description": request.form.get("description"),
+        "icon": request.form.get("icon"),
+        "color": request.form.get("color"),
+    }
+    category.create(data)
+    return redirect("/categories/")
 
 
-@category_blueprint.post("/update-category")
-def update_category():
+@category_blueprint.route("/category/<id>", methods=["GET", "POST"])
+def update_category(id):
+    if request.method == "GET":
+        kwargs = {"category": category.find_by_id(id)}
+        return render_template("/categories/show_category.html", **kwargs)
     data = {
         "id": request.form.get("id"),
         "name": request.form.get("name"),
